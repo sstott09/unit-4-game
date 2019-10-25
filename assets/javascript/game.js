85d1a359a94f979f58e4608620abb793c66bfa36
Steven Stot <sstott09@yahoo.com>
2019-10-25 03:50:30 -0600

    Updated with jQuery Functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -60,33 +60,79 @@ planet.red.value = getRandom(1,12);
 planet.purple.value = getRandom(1,12);
 
 //change the HTML to reflect all of these changes
+$("#yourScore").html(currentScore);
+$("#targetScore").html(targetScore);
+
 
 // Testing Console
 console.log("...................................")
 console.log("Target Score: " + targetScore);
-console.log("Blue: " + planet.blue.value + " | Green: " + planet.green.value + " | Red: " + planet.red.value + " | Purple: " + planet.purple.value)
+console.log("Blue: " + planet.blue.value + " | Red: " + planet.red.value + " | Green: " + planet.green.value + " |  Purple: " + planet.purple.value)
 console.log("...................................")
 
+}
+
+// Respond to click on the planets
+var addValues = function(planet) {
+
+// Change Current Score
+currentScore = currentScore + planet.value;
+
+// Change the HTML to reflect changes in currentScore
+$("#yourScore").html(currentScore);
+
+//Call the checkWin Function
+checkWin();
+
+// Testing Console
+console.log("Your Score: " + currentScore);
+}
+
+// Check if User Won or Lost and Reset the Game
+var checkWin = function() {
+
+    // Check if currentScore is larger than targetScore
+    if(currentScore > targetScore) {
+        alert("You Lost, better luck next time")
+        console.log("You Lost");
+
+        //Add to Loss Counter
+        lossCount++;
+
+        // Change Loss Count HTML
+        $("#lossCount").html(lossCount);
+    }
+
+    else if (currentScore == targetScore) {
+        alert("Nicely done, You Won!")
+        console.log("You Won!");
+
+        //Add to Win Counter
+        winCount++;
+
+        // Change Win Count HTML
+        $("#winCount").html(winCount);
+    }
 }
 
 // Main Process
 //.................................................................
 
-// Starts the Game the First Time
+// Starts the Game for the First Time
 startGame();
 
 $("#blue").click(function() {
-    alert("test");
-});
-
-$("#green").click(function() {
-    alert("test");
+    addValues(planet.blue);
 });
 
 $("#red").click(function() {
-    alert("test");
+    addValues(planet.red);
+});
+
+$("#green").click(function() {
+    addValues(planet.green);
 });
 
 $("#purple").click(function() {
-    alert("test");
+    addValues(planet.purple);
 });
